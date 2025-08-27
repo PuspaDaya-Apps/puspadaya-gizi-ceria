e719abd6176a562b14f8e960e1cc7cd6d435dca5
ajihermansya <ajihermansya14@gmail.com>
2025-08-27 12:46:52 +0700

    back-stagging

--- vite.config.ts
+++ vite.config.ts
@@ -12,36 +12,36 @@ export default defineConfig(({ mode }) => ({
     // 🔥 Tambahin proxy di sini
     proxy: {
       "/api": {
-        target: "http://gsg4k8os8gwsc4ksk4kkgg48.103.109.210.102.sslip.io",
+        target: "http://now4kswkgo4owoks884o0wc0.103.109.210.102.sslip.io",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, "/api/v1/public-dashboard"),
       },
       "/balita": {
-        target: "http://gsg4k8os8gwsc4ksk4kkgg48.103.109.210.102.sslip.io",
+        target: "http://now4kswkgo4owoks884o0wc0.103.109.210.102.sslip.io",
         changeOrigin: true,
         rewrite: (path) =>
           path.replace(/^\/balita/, "/api/v1/public-dashboard/balita-status"),
       },
       "/pengguna": {
-        target: "http://gsg4k8os8gwsc4ksk4kkgg48.103.109.210.102.sslip.io",
+        target: "http://now4kswkgo4owoks884o0wc0.103.109.210.102.sslip.io",
         changeOrigin: true,
         rewrite: (path) =>
           path.replace(/^\/pengguna/, "/api/v1/public-dashboard/kader-data"),
       },
       "/data-wilayah": {
-        target: "http://gsg4k8os8gwsc4ksk4kkgg48.103.109.210.102.sslip.io",
+        target: "http://now4kswkgo4owoks884o0wc0.103.109.210.102.sslip.io",
         changeOrigin: true,
         rewrite: (path) =>
           path.replace(/^\/data-wilayah/, "/api/v1/public-dashboard/wilayah-data"),
       },
         "/gizi-desa": {
-        target: "http://gsg4k8os8gwsc4ksk4kkgg48.103.109.210.102.sslip.io",
+        target: "http://now4kswkgo4owoks884o0wc0.103.109.210.102.sslip.io",
         changeOrigin: true,
         rewrite: (path) =>
           path.replace(/^\/gizi-desa/, "/api/v1/public-dashboard/status-gizi-desa"),
       },
         "/data-skdn": {
-        target: "http://gsg4k8os8gwsc4ksk4kkgg48.103.109.210.102.sslip.io",
+        target: "http://now4kswkgo4owoks884o0wc0.103.109.210.102.sslip.io",
         changeOrigin: true,
         rewrite: (path) =>
           path.replace(/^\/data-skdn/, "/api/v1/public-dashboard/skdn-data"),
